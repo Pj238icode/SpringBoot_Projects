102d77282cd7b0dd4ad1777d54440112b30d254d
Prasanjeet_Jena <122697153+Pj238icode@users.noreply.github.com>
2025-10-12 13:04:51 +0530

    Delete src directory

--- src/main/java/com/boot/backend/ContactManager/services/ImageService.java
+++
@@ -1,7 +0,0 @@
-package com.boot.backend.ContactManager.services;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface   ImageService {
-    public String uploadFile(MultipartFile file);
-}
