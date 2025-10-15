4052d70c06d42f9999d265dac38985f5d0be6ab8
Prasanjeet_Jena <122697153+Pj238icode@users.noreply.github.com>
2025-10-15 14:36:51 +0530

    Delete src directory

--- src/main/java/com/boot/backend/food_delivery_app/repository/FoodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boot.backend.food_delivery_app.repository;
-
-import com.boot.backend.food_delivery_app.entity.FoodEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FoodRepository extends MongoRepository<FoodEntity, String> {
-}
--- src/main/java/com/boot/backend/food_delivery_app/service/AutthenticationFacade.java
+++
@@ -1,8 +0,0 @@
-package com.boot.backend.food_delivery_app.service;
-
-import org.springframework.security.core.Authentication;
-
-public interface AutthenticationFacade {
-
-    Authentication getAuthentication();
-}
